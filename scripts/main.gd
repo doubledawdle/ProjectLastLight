5ffa847bddca99fd978bad4fa4ca56cbb87036b8
njandersen <njandersen22@gmail.com>
2024-09-13 21:03:37 -0400

    Feat: add main menu and fix bugs

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,6 +6,7 @@ var notification_timer: Timer = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	LocationStateManager.connect("resource_added", Callable(self, "_on_resource_added"))
+	GameStateManager.connect("update_timer", Callable(self, "_on_update_timer"))
 	
 	# Create a Timer node for handling when the text disappears
 	notification_timer = Timer.new()
@@ -28,3 +29,13 @@ func _on_resource_added(resource_name: String, amount: int) -> void:
 	
 func _hide_notification() -> void:
 	$NotificationLabel.visible = false
+	
+func _on_update_timer(time_left: float) -> void:
+	var curr_cycle = GameStateManager.get_current_cycle()
+	var timer_label = $Timer  
+	if time_left > 0:
+		var minutes = int(time_left) / 60
+		var seconds = int(time_left) % 60
+		timer_label.text = GameStateManager.get_cycle_name(curr_cycle) + ": " + str(minutes).pad_zeros(2) + ":" + str(seconds).pad_zeros(2)
+	else:
+		timer_label.text = "Time's up!"
